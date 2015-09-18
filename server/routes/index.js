07b438c1db0a9dbcb531aaffc2f6a2952d49be87
Hooman <yar00001@umn.edu>
2015-09-18 18:15:15 -0500

    setting res for POST requests

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,25 +3,22 @@ var router = express.Router();
 var lib = require('./../../lib')
 
 /* GET / */
-router.get('/', function(req, res, next) {
-	res.send('express home page');
-	next();
+router.get('/', function(req, res) {
+	res.send('<h1>express home page</h1>');
+
 })
 
 /* POST / */
-router.post('/', function(req, res, next) {
+router.post('/', function(req, res) {
 	// only accepts 'application/json'
 	if (req.headers['content-type'] != 'application/json'){
 		console.log('you need to send application/json');
 		res.sendStatus(415);
 	}
-	res.send('got the post');
-	/**
-	 * writing a json file in root directory. just for testing and debugging
-	 */
-	// lib.makeJsonFile(req.body);
+	
 	console.log(req.body);
-	next();
+	res.send('got the post');
+
 })
  
 
