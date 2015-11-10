7ee0a4394d5c95b1548655fe201487bfbefe79db
hooman <yar00001@umn.edu>
2015-11-10 14:17:10 -0600

    clustering the current app

--- server/app.js
+++ server/app.js
@@ -2,10 +2,8 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
-var http = require('http');
-//routing modules
-var async = require('async');
-var lib = require('./../lib');
+// routings
+var routes = require('./routes/index');
 
 /**
  * using bodyParser
@@ -21,93 +19,12 @@ app.use(function(req, res, next) {
   next();
 });
 
-/**
- * defining routes
- * ToDo: find a way to make 'route' compatible with iisnode by adding routes to web.config watch list
- */
-/* GET / */
-app.get('/nodeprint/server', function(req, res) {
-    res.send('<h1>Express Home Page</h1><p>Send your post req to the url</p><P>More Information <a href="https://github.umn.edu/hooman/NodePrintApplication">Here</a></P>');
-});
-app.get('/nodeprint/server/test', function(req, res) {
-    // testing the builder
-    res.send(lib.appBuilder.appTestBuilder());
+app.use('/', routes);
 
-});
-
-/* POST / */
-app.post('/nodeprint/server/', function(req, res) {
-    // only accepts 'application/json'
-    if (req.headers['content-type'] != 'application/json'){
-        console.log('you need to send application/json');
-        res.sendStatus(415);
-    }
-
-    async.waterfall([
-        function makeHtml(makeHtmlCallback) {
-            lib.appBuilder.appBuilder(function(err, html) {
-                if (err) console.log(err);
-                makeHtmlCallback(null, html);
-            }, req.body.json, req.body.temp);
-        },
-        function sendRes(html, sendCallback) {
-            res.send({msg: "done", html: html});
-            sendCallback();
-        }
-    ], function(err) {
-        if (err) {
-            consoel.log(err);
-            res.sendStatus(500);
-        }
-    });
-});
-
-/**
- * making test-template for POST req
- */
-app.post('/nodeprint/server/test', function(req, res) {
-    // only accepts 'application/json'
-    if (req.headers['content-type'] != 'application/json'){
-        console.log('you need to send application/json');
-        res.sendStatus(415);
-    }
-
-    async.waterfall([
-        function makeHtml(makeHtmlCallback) {
-            lib.appBuilder.appBuilder(function(err, html) {
-                if (err) console.log(err);
-                makeHtmlCallback(null, html);
-            }, req.body, "test");
-        },
-        function sendRes(html, sendCallback) {
-            res.send({msg: "done", html: html});
-            sendCallback();
-        }
-    ], function(err) {
-        if (err) {
-            consoel.log(err);
-            res.sendStatus(500);
-        }
-    });
-});
 
 /**
  * manage static files
  */
 app.use(express.static(path.join(__dirname, 'tmp')));
 
-/**
- * PORT
- * default or 3000
- */
-var port = process.env.PORT || '3000';
-app.set('port', port);
-
-/**
- * making http server
- */
-var server = http.createServer(app);
-server.listen(port);
-
-
-// module.exports = app;
+module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,35 +1,28 @@
 var express = require('express');
 var router = express.Router();
-var lib = require('./../../lib');
 var async = require('async');
+var lib = require('./../../lib');
 
-/**
- * IS NOT IMPLEMENTED:
- * EVERYTHING IS MOVED TO APP.JS
- * TODO: MAKE IT WORK WITH IISNODE
- */
-/* GET / */
+/* GET home page. */
 router.get('/', function(req, res) {
-	res.send('<h1>Express Home Page</h1><p>send your post req to this urldssd</p>');
-    // delete the file
+    res.sendFile(__dirname + '/index.html');
 });
-
 
 
 /* POST / */
 router.post('/', function(req, res) {
-	// only accepts 'application/json'
-	if (req.headers['content-type'] != 'application/json'){
-		console.log('you need to send application/json');
-		res.sendStatus(415);
-	}
+    // only accepts 'application/json'
+    if (req.headers['content-type'] != 'application/json'){
+        console.log('you need to send application/json');
+        res.sendStatus(415);
+    }
 
     async.waterfall([
         function makeHtml(makeHtmlCallback) {
             lib.appBuilder.appBuilder(function(err, html) {
                 if (err) console.log(err);
                 makeHtmlCallback(null, html);
-            }, req.body, "default");
+            }, req.body.json, req.body.temp);
         },
         function sendRes(html, sendCallback) {
             res.send({msg: "done", html: html});
@@ -37,40 +30,10 @@ router.post('/', function(req, res) {
         }
     ], function(err) {
         if (err) {
-            consoel.log(err);
+            console.log(err);
             res.sendStatus(500);
         }
     });
-});
-
-/**
- * making test-template
- */
-router.post('/test', function(req, res) {
-	// only accepts 'application/json'
-	if (req.headers['content-type'] != 'application/json'){
-		console.log('you need to send application/json');
-		res.sendStatus(415);
-	}
-
-    async.waterfall([
-        function makeHtml(makeHtmlCallback) {
-            lib.appBuilder.appBuilder(function(err, html) {
-                if (err) console.log(err);
-                makeHtmlCallback(null, html);
-            }, req.body, "test");
-        },
-        function sendRes(html, sendCallback) {
-            res.send({msg: "done", html: html});
-            sendCallback();
-        }
-    ], function(err) {
-        if (err) {
-            consoel.log(err);
-            res.sendStatus(500);
-        }
-    });
-
 });
 
 module.exports = router;
